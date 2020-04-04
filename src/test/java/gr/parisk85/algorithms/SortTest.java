4a62069cc26b133338a538dda12629598dbd3ead
Paris Karagiannopoulos <parisk85@gmail.com>
2020-04-05 01:00:50 +0300

    Merge sort tests

--- src/test/java/gr/parisk85/algorithms/SortTest.java
+++ src/test/java/gr/parisk85/algorithms/SortTest.java
@@ -11,4 +11,16 @@ public class SortTest {
         Integer[] actual = Sort.merge(38, 27, 43, 3, 9, 82, 10);
         assertArrayEquals(new Integer[]{3, 9, 10, 27, 38, 43, 82}, actual);
     }
+
+    @Test
+    public void mergeShouldReturnEmptyArrayWhenNoInput() {
+        Integer[] actual = Sort.merge();
+        assertArrayEquals(new Integer[]{}, actual);
+    }
+
+    @Test
+    public void mergeShouldReturnOneElementArrayWhehnOneArg() {
+        String[] actual = Sort.merge("one argument");
+        assertArrayEquals(new String[]{"one argument"}, actual);
+    }
 }
